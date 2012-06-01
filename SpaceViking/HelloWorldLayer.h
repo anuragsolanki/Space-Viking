1c3a911554c60e991e671346ff8fcd807f055bf5
Anurag Solanki <anurag@vinsol.com>
2012-06-01 10:30:16 +0530

    Initial Commit

---
+++ SpaceViking/HelloWorldLayer.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldLayer.h
+//  SpaceViking
+//
+//  Created by Anurag Solanki on 01/06/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ SpaceViking/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  SpaceViking
+//
+//  Created by Anurag Solanki on 01/06/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
